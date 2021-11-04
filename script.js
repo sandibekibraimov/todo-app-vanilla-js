2a80fc9207deb86c3a2435ded22bfb2af8cd9466
sandibekibraimov <sandibek@gmail.com>
2021-11-04 01:56:31 -0400

    added delete function

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
 
+const generateTemplate = (todo) => {
+  const html = `
+        <li
+          class="
+            list-group-item
+            d-flex
+            justify-content-between
+            align-items-center
+          "
+        >
+          <span>${todo}</span>
+          <i class="far fa-trash-alt delete"></i>
+        </li>
+    `;
+
+  list.innerHTML += html;
+};
+
+// add new todo
+addForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const todo = addForm.add.value.trim();
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
+
+// delete todo
+list.addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
+  }
+});
